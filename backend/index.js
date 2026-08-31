6ef373d504d59ff7bf13e386b252f3db66c69e73
Shivran-git <shivran5028@gmail.com>
2026-08-31 22:47:50 +0530

    MongoDB Atlas , cookie implement, logout route

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,17 @@ import connectDB from "./config/db.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from 'cookie-parser';
 
+const PORT = process.env.PORT || 5000;
 dotenv.config();
-connectDB();
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Mini PAIMANA API is running...");
 });
@@ -24,5 +26,11 @@ app.use("/api/projects", projectRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get(/.*/, (req, res) => {                  // in order to handle random api calls
+       return res.json({message : "not found"})
+    });
+
+app.listen(PORT, () =>{
+  connectDB();
+  console.log("server is running")
+});
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { registerUser, loginUser, getProfile } from "../controllers/authController.js";
+import { registerUser, loginUser, getProfile, logoutController } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post('/logout',logoutController);
 router.get("/me", protect, getProfile);
 
 export default router;
